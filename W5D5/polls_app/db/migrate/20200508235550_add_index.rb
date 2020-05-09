a38ed59d21ea44a030ddb7551dbbcc14984e0435
Ben Rose <benrose207@gmail.com>
2020-05-08 18:37:37 -0700

    Work on W5D5 classwork

---
+++ W5D5/polls_app/db/migrate/20200508235550_add_index.rb
@@ -0,0 +1,10 @@
+class AddIndex < ActiveRecord::Migration[5.2]
+  def change
+      add_index :answer_choices, :question_id 
+      add_index :polls, :author_id
+      add_index :questions, :poll_id 
+      add_index :responses, :user_id 
+      add_index :responses, :answer_choice_id 
+    
+  end
+end
